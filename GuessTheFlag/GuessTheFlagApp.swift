41d8a5d9226e0f1ff7ee3f79ab4ff66a7ad5f9cf
Razvan Pricop <razvanipricop@gmail.com>
2024-10-01 15:09:56 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Razvan Pricop on 01.10.24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
